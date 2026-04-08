16f333192fb894ee656ce894d8ac5886e5b4fac9
Ankushkumardas <anksuh114@gmail.com>
2026-04-09 01:02:01 +0530

    learnings

---
+++ python/input_type_conversion.py
@@ -0,0 +1,15 @@
+# input()--> by default goes in string format
+# a=input("input bol")
+# a=input(prompt="input bol")
+# print("a input is -->",a,type(a))
+
+# a=int(input("A -->"))
+# b=int(input("B -->"))
+# print(a+b)
+
+
+# type conversion-->implivit and explicit
+print(type("sca"))
+print(list("sajcb"))
+print(tuple("sajcb"))
+print(set("sajcb"))
---
+++ python/literals_operators.py
@@ -0,0 +1,54 @@
+# literals-->raw data given to a variable in python
+
+# string literals-->
+# multiline string in --? triple interved comm's we take  """ scbmas"""
+# raw string to print html ijn python we can do as-->r"raw \n string"
+
+# numeric literals-->
+# int-->
+# float-->
+# complex-->
+
+# boolean literals-->
+a=True+1
+print(a)
+# special literals-->None
+
+
+# operators--->
+# arithmetic operators-->   
+# +,-,*,/,%,**,//
+
+# relational operators-->
+# <,>,<=,>=,==,!=    
+
+# logical operators-->
+# and,or,not    
+
+# bitwise operators-->
+# &,|,^,~,<<,>>    
+
+# assignment operators-->
+# =,+=,-=,*=,/=,%=,**=,//=
+
+# membership operators-->
+# in,not in    
+
+# identity operators-->
+# is,is not    
+
+# ternary operators-->
+# a if condition else b    
+
+# operator precedence-->
+# ()-->parenthesis
+# **-->exponent
+# *,/,//,%-->multiplication,division,floor division,modulo
+# +,--->addition,subtraction
+# <,>,<=,>=,==,!=-->relational operators
+# and,or,not-->logical operators
+# in,not in-->membership operators
+# is,is not-->identity operators
+# &,|,^,~,<<,>>-->bitwise operators
+# =,+=,-=,*=,/=,%=,**=,//=-->assignment operators
+# a if condition else b-->ternary operators
