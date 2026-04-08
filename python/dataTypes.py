b69ed8942072c0eedec21d5ef40c968de4e553b4
Ankushkumardas <anksuh114@gmail.com>
2026-04-08 23:58:57 +0530

    learning basics

---
+++ python/dataTypes.py
@@ -0,0 +1,20 @@
+# basic--> interger,float,complex,boolean,string
+# container type-->{ List->like array,Tuple,Sets and Dictonary-> key value pairs}
+# user defined-->Class
+
+# list
+print([1,2,3,4,5])
+# tuple
+print((1,2,3,4,5))
+# sets
+print({1,1,2,3,4,4,5})
+# dictonary
+print({1:"a",2:"b",3:"c",4:"d",5:"e"})
+
+
+# variables-->
+# dynamic typing--> in python we do not have to tell what type of data type we are using python is smart enough and automaticc  assign teh data type to teh value you have set 
+# static typeing --> in all other languages we have to tell what type of data type we have to use 
+#  and dynamic binding
+a="kajslkcn"
+print(a)
---
+++ python/test.py
@@ -0,0 +1,4 @@
+print("test")
+print("test","jscbkj","dvsjb","cs")
+print("kmsac","scsa","csdc",sep="|")
+print("kmsac","scsa","csdc",end="\n")
