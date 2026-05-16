e2408b73734ecb8e51e3f76d66a8e1fbe6f51adb
Ankushkumardas <anksuh114@gmail.com>
2026-05-17 00:56:14 +0530

    changes in list

---
+++ list_in_pythin.py
@@ -0,0 +1,1 @@
+# what are lists in python
--- python/test.py
+++ python/test.py
@@ -1,4 +1,38 @@
-print("test")
-print("test","jscbkj","dvsjb","cs")
-print("kmsac","scsa","csdc",sep="|")
-print("kmsac","scsa","csdc",end="\n")
+# print("test")
+# print("test","jscbkj","dvsjb","cs")
+# print("kmsac","scsa","csdc",sep="|")
+# print("kmsac","scsa","csdc",end="\n")
+
+# string are immutable or add or delete in python not editable 
+# a[0]="A"
+
+# a=int(input("enter a number : "))
+# b=50
+# while a!=b:
+#     if a>b:
+#         print("gess lower")
+#     elif a<b:
+#         print("gess higher")
+#     a=int(input("enter teh number to gesss :"))
+# print("you gess it :",b)
+
+# a="sfsfsa jgj sjbjb ss"
+# print(len(a))
+# print(max(a))
+# print(sorted(a,reverse=True))
+
+# print(a.capitalize())
+# print(a.lower())
+# print(a.upper())
+# print(a.title())
+
+# print(a.find('ss'))
+# print(a.index('ss'))
+
+# print(a.endswith("ss"))
+# print(a.startswith("jgj"))
+
+# a="jdsb sdcjsn {} ksjc {} ich cdsjn"
+print('csjb {} s cjc {}'.format(1222,123333))
+print('csjb {name} s cjc {age}'.format(name="janki",age=123333))
+print("sac34".isalnum())
