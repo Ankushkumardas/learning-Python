6d8cf35135f34b21fd598a752c6a9446947dd1bf
Ankushkumardas <anksuh114@gmail.com>
2026-06-01 23:21:26 +0530

    fast api learning

--- python/Fast_api/patient.py
+++ python/Fast_api/patient.py
@@ -1,6 +1,6 @@
 from fastapi import status
 import json
-from fastapi import FastAPI,Path,HTTPException
+from fastapi import FastAPI,Path,HTTPException,Query
 
 app=FastAPI()
 
@@ -15,6 +15,7 @@ def get_data():
 
 @app.get("/view")
 def get_all_data():
+    # data=load_data()
     return load_data()
 
 # path and query paarmeters -->
@@ -24,6 +25,23 @@ def get_patient_by_id(id:str=Path(...,description="id od teh patient P001")):
     data=load_data()
     for key in data:
         if key==id:
-            return data[key]
+            return data[key]["name"],data[key]["age"]
     # return {"error":"no data found"}
         raise HTTPException(status_code=404,detail="pateinet not found")
+
+# query parameter--> ... dots in side teh query means that fiedl is required to put value 
+@app.get('/sort')
+def sort_data(sort_by:str = Query(...,description="description for sort"),order:str =Query(...,description="ascending or descrnfding order")):
+    valid_fields=['height','weight']
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=404,detail="invalid sort fiedl for query")
+    if order not in ['asc','desc']:
+        raise HTTPException(status_code=404,detail="invalid order fiedl for query")
+    data=load_data()
+    sort_order=True if order=="desc" else False
+    print(data.values())
+    sorted_data=sorted(data.values(),key=lambda x:x.get(sort_by),reverse=sort_order)
+    return sorted_data
+
+     
+# sorted() is a built-in Python function that returns a new sorted list from an iterable. By default it sorts in ascending order. The key parameter allows custom sorting logic, and reverse=True sorts in descending order.Syntax ---> sorted(iterable, key=None, reverse=False) --->sorted(products, key=lambda x: x["price"], reverse=True)
